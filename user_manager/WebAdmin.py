f06d9c2e4fb8b19e2940fd17c8d2cf72ee6cafdf
EugeneK6 <kotbajun03@gmail.com>
2024-04-12 01:03:08 +0300

    Adding ai code reviewer

--- user_manager/WebAdmin.py
+++ user_manager/WebAdmin.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-import psycopg2
 import os
 import logging
+from flask import Flask, render_template, request, redirect, url_for, flash
+import psycopg2
 from logfmter import Logfmter
 
 # Configure logging

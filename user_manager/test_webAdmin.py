c2e0532ad468dd08d98d2377dabf0ea958565faa
EugeneK6 <yevhenii.kyselov@kneu.ua>
2024-05-26 02:42:42 +0300

    try unittest.mock in unit-test

--- user_manager/test_webAdmin.py
+++ user_manager/test_webAdmin.py
@@ -1,25 +1,26 @@
 """This module contains the unit tests for the user_manager module."""
 
 import unittest
-from psycopg2.extensions import connection
-from psycopg2 import OperationalError
+from sqlite3 import OperationalError
+from unittest.mock import patch
 from WebAdmin import get_db_connection
-
 
 class TestGetDBConnection(unittest.TestCase):
     """This class contains the unit tests for the `get_db_connection` function."""
 
-    def test_get_db_connection_success(self):
+    @patch('WebAdmin.psycopg2.connect')
+    def test_get_db_connection_success(self, mock_connect):
         """This test case tests the successful connection to the database."""
+        mock_conn = mock_connect.return_value
         conn = get_db_connection()
-        self.assertIsInstance(conn, connection)
+        self.assertEqual(conn, mock_conn)
         conn.close()
 
-    def test_get_db_connection_failure(self):
+    @patch('WebAdmin.psycopg2.connect', side_effect=OperationalError("Connection failed"))
+    def test_get_db_connection_failure(self, mock_connect):
         """This test case tests the failure of the connection to the database with an invalid host."""
         with self.assertRaises(OperationalError):
             get_db_connection(host='invalid_host')
-
 
 if __name__ == '__main__':
     unittest.main()
